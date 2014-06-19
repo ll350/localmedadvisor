93a21784ddb36f0927a86aa4808586d00ebe9700
LeoLanzo <llanzo@ncha.org>
2014-06-19 16:58:51 -0400

    Added some data and created procedures in seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+# csv_users_text = File.read('db/DemoUsers.csv')
+# csv_users = CSV.parse(csv_users_text, :headers => true)
+# csv_users.each do |usee|
+#   newuser = User.new(email: usee['email'], password: usee['password'], password_confirmation: usee['password'] )
+#   newuser.save(false)
+#   #puts usee['email']
+#
+# end
+
+#puts 'hello jackass'
+csv_pros_text = File.read('db/DemoGroupings.csv')
+csv_pros = CSV.parse(csv_pros_text, :headers => true)
+csv_pros.each do |pro|
+  Procedure.create(name: pro['CmsSpecialty'], description: pro['SpecialtyGrouping'])
+end
